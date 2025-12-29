3d3f0b74a721e481b5c17729b471c79344429ddd
OmRana2004 <omrana8755800322@gmail.com>
2025-12-29 19:05:05 +0530

    fix3

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 let prisma: PrismaClient | null = null;
 
 export function getPrisma() {
   if (!prisma) {
-    prisma = new PrismaClient();
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    const adapter = new PrismaPg(pool);
+    prisma = new PrismaClient({ adapter });
   }
+
   return prisma;
 }
--- db/prisma.config.ts
+++ db/prisma.config.ts
@@ -2,13 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  // paths are RELATIVE TO THIS FILE (db/)
   schema: "prisma/schema.prisma",
-
-  migrations: {
-    path: "prisma/migrations",
-  },
-
   datasource: {
     url: process.env.DATABASE_URL,
   },
