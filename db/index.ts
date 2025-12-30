c6d60ec2f3bd96191248ffa8d5e82deca60633a5
OmRana2004 <omrana8755800322@gmail.com>
2025-12-30 19:58:34 +0530

    fix1

--- db/index.ts
+++ db/index.ts
@@ -1,34 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
-  pgPool?: Pool;
 };
 
 export function getPrisma() {
-  if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not defined");
-  }
-
-  if (!globalForPrisma.pgPool) {
-    globalForPrisma.pgPool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false }
-          : false,
-    });
-  }
-
   if (!globalForPrisma.prisma) {
-    const adapter = new PrismaPg(globalForPrisma.pgPool);
     globalForPrisma.prisma = new PrismaClient({
-      adapter,
       log: ["error"],
     });
   }
-
   return globalForPrisma.prisma;
 }
