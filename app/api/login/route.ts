a8fa20d5fa1308d4d46ea164a78d89742b6b5562
OmRana2004 <omrana8755800322@gmail.com>
2026-01-21 15:50:49 +0530

    admin id pass add into env

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextResponse } from "next/server";
 
 const ADMIN_EMAIL = "admin@mps.com";
@@ -5,6 +6,13 @@ const ADMIN_PASSWORD = "Admin@2026";
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
+
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
+  if (!ADMIN_EMAIL || !ADMIN_PASSWORD) {
+    return NextResponse.json({ error: "server Error" }, { status: 500 });
+  }
 
   if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
     const res = NextResponse.json({ success: true });
@@ -17,8 +25,5 @@ export async function POST(req: Request) {
     return res;
   }
 
-  return NextResponse.json(
-    { error: "Invalid credentials" },
-    { status: 401 }
-  );
+  return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
 }
